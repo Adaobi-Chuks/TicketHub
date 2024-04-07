82ccf2e6bb0a7fc66d1227d0e27aa543ca623668
Adaobi-Chuks <chuksaginamada@gmail.com>
2024-04-07 13:58:06 -0800

    Feat: Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react"
 import './App.css';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route
+} from "react-router-dom"
 import LandingPage from '@/pages/landingPage'
+import SignUpPage from '@/pages/signupPage'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/">
+    <Route index element={<LandingPage />} />
+    <Route
+      path="signup"
+      element={<SignUpPage />}
+    />
+  </Route>
+))
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <RouterProvider router={router} />
   )
 }
 
