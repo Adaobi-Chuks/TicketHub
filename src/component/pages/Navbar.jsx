0e65c9da10c3765c5e918f44cf9150d94c629609
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-04-09 03:28:55 +0000

    double

--- src/component/pages/Navbar.jsx
+++ src/component/pages/Navbar.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, {useContext, useState } from 'react';
 import Logo from '../../assets/logo.png';
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import { AuthContext } from '../../AuthContext';
+import IMG from '../../assets/twitter.png'
 
 const Navbar = () => {
+  const {user} = useContext(AuthContext);
   const [nav, setNav] = useState(true);
 
     const handleNav = () => {
@@ -15,13 +18,16 @@ const Navbar = () => {
       <img src={Logo} alt='/' className='w-[6rem] h-[4rem]'/></a>
     </div>
     <div className='flex gap-8'>
-      <ul className='text-white gap-8 items-center  md:flex hidden'>
+      {user ? (
+      <>
+        <img src={IMG} alt='' className='w-[5rem] h-[5rem]'/>
+        <p>{user.name}</p>
+      </>
+    ) : (<ul className='text-white gap-8 items-center  md:flex hidden'>
         <li className=' text-white '><a href='/Login'>Login</a></li>
         <li className='bg-[#0D2986] p-2 text-white rounded-sm'><a href='/Register'>Sign Up</a></li>
-      </ul>
-      <div className='grid'>
-        
-      </div>
+      </ul>)}
+     
     </div>
 
     <div onClick={handleNav} className='block md:hidden text-white'>
@@ -29,10 +35,17 @@ const Navbar = () => {
     </div>
 
     <div className={!nav ? 'fixed left-0 top-0 w-[50%] h-[60%]  border-r-gray-900 bg-[#110eca] z-10 ease-in-out duration-500': 'fixed left-[-400%]'}>
-       <ul className=' text-color grid items-center justify-center uppercase pt-24 '>
-       <li className=' text-white '><a href='/Login'>Login</a></li>
-        <li className='bg-[#0D2986] p-2 text-white rounded-sm'><a href='/Register'>Sign Up</a></li>
-       </ul>
+    {user ? (
+      <>
+        <img src={IMG} alt='' className='w-[5rem] h-[5rem]'/>
+        <p>{user.name}</p>
+      </>
+    ) : ( <ul className=' text-color grid items-center justify-center uppercase pt-24 '>
+    <li className=' text-white '><a href='/Login'>Login</a></li>
+     <li className='bg-[#0D2986] p-2 text-white rounded-sm'><a href='/Register'>Sign Up</a></li>
+    </ul>
+  )}
+      
     </div>
  
   </div>
